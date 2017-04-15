e4776f90eaabb46737471512d9a2eac4f824f372
Andrija Čehko <andrija.cehko@gmail.com>
2017-04-15 19:57:59 +0200

    Add syntax highlighting

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,7 +1,9 @@
 # --- Antigen Start ---
 source $ZSH_PATH/antigen/antigen.zsh
 
+
 antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-syntax-highlighting
 
 antigen apply
 # --- Antigen End ---
@@ -11,9 +13,32 @@ antigen apply
 # --- Plugin Config Start ---
 
 
+
 # Autosuggest
 bindkey '^ '   autosuggest-accept  # Bind CTRL + Space to accept the curretn suggestion
 bindkey '^[^M' autosuggest-execute # Bind ALT  + Enter to execute the current suggestion
+
+
+# Syntax Highlighting
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
+
+# Main
+ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=red
+ZSH_HIGHLIGHT_STYLES[precommand]=fg=none
+ZSH_HIGHLIGHT_STYLES[commandseparator]=fg=yellow
+ZSH_HIGHLIGHT_STYLES[path]=none
+ZSH_HIGHLIGHT_STYLES[globbing]=none
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=fg=green
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=green
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=green
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=none
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=none
+ZSH_HIGHLIGHT_STYLES[redirection]=fg=yellow
+ZSH_HIGHLIGHT_STYLES[arg0]=fg=blue
+
+# Brackets
+ZSH_HIGHLIGHT_STYLES[bracket-error]=fg=red
+
 
 
 # --- Plugin Config End ---
