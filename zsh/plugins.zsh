40360eb28c04ef583f199cc36be6981f920eeec2
Andrija Čehko <andrija.cehko@gmail.com>
2017-04-15 16:08:12 +0200

    Add autosuggestions

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,3 +1,19 @@
+# --- Antigen Start ---
 source $ZSH_PATH/antigen/antigen.zsh
 
+antigen bundle zsh-users/zsh-autosuggestions
+
 antigen apply
+# --- Antigen End ---
+
+
+
+# --- Plugin Config Start ---
+
+
+# Autosuggest
+bindkey '^ ' autosuggest-accept  # Bind CTRL + Space to accept the curretn suggestion
+bindkey '^M' autosuggest-execute # Bind CTRL + Enter to execute the current suggestion
+
+
+# --- Plugin Config End ---
