303bf38c62d955be9091cb3fddf75df0eaf1ad20
Andrija Čehko <andrija.cehko@gmail.com>
2017-04-15 15:43:12 +0200

    Zsh general config

---
+++ zsh/general.zsh
@@ -0,0 +1,13 @@
+# General config
+
+setopt nobeep      # Disable beep on errors
+setopt autocd      # Writing a directory name will cd into it
+setopt extendedglob # Enables extra file matchig patterns
+setopt multios      # Allows redirecting output to multiple files
+setopt correct      # Prompt misspelled command correction
+
+# Fix HOME and END keys
+bindkey '\e[1~' beginning-of-line
+bindkey '\e[4~' end-of-line
+
+bindkey -v # Use vi as the line editor
