331659756824a774774f27a0df8ee9b16a0aa951
Andrija Čehko <andrija.cehko@gmail.com>
2017-04-19 13:51:21 +0200

    Reformat code

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,24 +1,33 @@
 setopt prompt_subst
 autoload -Uz vcs_info
 
-zstyle ':vcs_info:*' enable git      # Enable git
-zstyle ':vcs_info:*' formats '%b/%S' # Load git branch ($vcs_info_msg_0_)
+zstyle ":vcs_info:*" enable git      # Enable git
+zstyle ":vcs_info:*" formats "%b/%S" # Load git branch ($vcs_info_msg_0_)
 
 
 
-# Left prompt
 precmd () {
 
-	# Run vcs info
 	vcs_info
+	left_prompt
+	right_prompt
+
+}
+
+
+# Draws the left prompt
+left_prompt() {
+
 
 	# Colors
-	color_ok='blue'
-	color_error='red'
-	color_branch='green'
+	color_ok="blue"
+	color_error="red"
+	color_branch="green"
+
 
 	# Show exclamation mark if running with admin privileges
-	PROMPT='%F{red}%(!. .)%f'
+	PROMPT="%F{red}%(!. .)%f"
+
 
 	# If in a git repository
 	if [[ -n ${vcs_info_msg_0_} ]]; then
@@ -26,7 +35,7 @@ precmd () {
 		# Git status dirty
 		git_status=$(command git status --porcelain 2> /dev/null | tail -n1)
 		if [[ -n $git_status ]]; then
-			color_branch='yellow'
+			color_branch="yellow"
 		fi
 
 		git_branch=$vcs_info_msg_0_
@@ -38,36 +47,50 @@ precmd () {
 
 	fi
 
+
 	PROMPT+="%(?.%F{$color_ok}.%F{$color_error})❯%f "
 
+
 }
 
 
+# Draws the right prompt
+right_prompt() {
 
-# Right prompt
 
-# Vim modes
-vim_ins_mode=""
-vim_cmd_mode=" %F{blue}[CMD]%f"
-vim_mode=$vim_ins_mode
+	# Colors
+	color_path="242"
+	color_cmd="blue"
 
-# Update vim mode on change
-function zle-keymap-select {
-	vim_mode="${${KEYMAP/vicmd/${vim_cmd_mode}}/(main|viins)/${vim_ins_mode}}"
-	zle reset-prompt
-}
-zle -N zle-keymap-select
-
-# Reset vim mode
-function zle-line-finish {
+	# Vim modes
+	vim_ins_mode=""
+	vim_cmd_mode=" %F{$color_cmd}[CMD]%f"
 	vim_mode=$vim_ins_mode
-}
-zle -N zle-line-finish
 
-# Reset vim mode on CTRL+C
-function TRAPINT() {
-	vim_mode=$vim_ins_mode
-	return $(( 128 + $1 ))
-}
 
-RPROMPT='%F{242}%~%f${vim_mode}'
+	# Update vim mode on change
+	function zle-keymap-select {
+		vim_mode="${${KEYMAP/vicmd/${vim_cmd_mode}}/(main|viins)/${vim_ins_mode}}"
+		zle reset-prompt
+	}
+	zle -N zle-keymap-select
+
+
+	# Reset vim mode
+	function zle-line-finish {
+		vim_mode=$vim_ins_mode
+	}
+	zle -N zle-line-finish
+
+
+	# Reset vim mode on CTRL+C
+	function TRAPINT() {
+		vim_mode=$vim_ins_mode
+		return $(( 128 + $1 ))
+	}
+
+
+	RPROMPT='%F{$color_path}%~%f$vim_mode'
+
+
+}
