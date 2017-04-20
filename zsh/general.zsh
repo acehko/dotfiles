f259fc7b9b5316dc74256ddbd7c5a24f0af6d4a3
Andrija Čehko <andrija.cehko@gmail.com>
2017-04-20 10:53:38 +0200

    Previous selection keybind

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -7,8 +7,12 @@ setopt multios             # Allows redirecting output to multiple files
 setopt correct             # Prompt misspelled command correction
 setopt interactivecomments # Allow comments in the shell
 
+
 # Enable completion
 autoload -Uz compinit && compinit
 
 # Enable completion menu selection
 zstyle ':completion:*' menu select
+
+# Bind SHIFT + TAB for previous selection
+bindkey '^[[Z' reverse-menu-complete
