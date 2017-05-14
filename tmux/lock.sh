98f8f839b8c4753e4e49ced6e35936cd3a611eae
Andrija Čehko <andrija.cehko@gmail.com>
2017-05-14 15:10:04 +0200

    Save session on idle

--- tmux/lock.sh
+++ tmux/lock.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 
+# Save tmux environment
+source $DOTFILES/tmux/session.sh save
+
 # Show asciiquarium only if not over ssh
 if [[ ! -n $SSH_CONNECTION ]]; then
 	asciiquarium
