775ac615cf9cc509c79e13b915751c0a23fa5e5b
Andrija Čehko <andrija.cehko@gmail.com>
2017-05-17 16:06:51 +0200

    Create neofetch config dir

--- scripts/neofetch.sh
+++ scripts/neofetch.sh
@@ -6,6 +6,13 @@ echo -en "\nLinking neofetch config... "
 if [ -e $HOME/.config/neofetch/config ]; then
 	echo "Already exists"
 else
+
+	# Create dir if not exists
+	if [ ! -e $HOME/.config/neofetch ]; then
+		mkdir -p $HOME/.config/neofetch
+	fi
+
 	ln -s $DOTFILES/neofetch/config $HOME/.config/neofetch/config
 	echo -en "\n"
+
 fi
