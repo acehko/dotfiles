03479c488a4578a95c252b05c49680fe7bfec04d
Andrija Čehko <andrija.cehko@gmail.com>
2017-05-22 16:19:55 +0200

    Zsh variable colors in strings

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -22,26 +22,26 @@ bindkey '^[^M' autosuggest-execute # Bind ALT  + Enter to execute the current su
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 
 # Main
-ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=167           # Red
-ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=215           # Yellow
+ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=167                 # Red
+ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=215                 # Yellow
 ZSH_HIGHLIGHT_STYLES[precommand]=fg=none
-ZSH_HIGHLIGHT_STYLES[commandseparator]=fg=215        # Yellow
+ZSH_HIGHLIGHT_STYLES[commandseparator]=fg=215              # Yellow
 ZSH_HIGHLIGHT_STYLES[path]=none
 ZSH_HIGHLIGHT_STYLES[globbing]=none
-ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=fg=107    # Green
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=107  # Green
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=107  # Green
-ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=none
-ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=none
-ZSH_HIGHLIGHT_STYLES[redirection]=fg=215             # Yellow
-ZSH_HIGHLIGHT_STYLES[arg0]=fg=110                    # Blue
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=fg=107          # Green
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=107        # Green
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=107        # Green
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=231 # White
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=231   #W hite
+ZSH_HIGHLIGHT_STYLES[redirection]=fg=215                   # Yellow
+ZSH_HIGHLIGHT_STYLES[arg0]=fg=110                          # Blue
 
 # Brackets
-ZSH_HIGHLIGHT_STYLES[bracket-error]=fg=167           # Red
-ZSH_HIGHLIGHT_STYLES[bracket-level-1]=fg=110         # Blue
-ZSH_HIGHLIGHT_STYLES[bracket-level-2]=fg=110         # Blue
-ZSH_HIGHLIGHT_STYLES[bracket-level-3]=fg=110         # Blue
-ZSH_HIGHLIGHT_STYLES[bracket-level-4]=fg=110         # Blue
-ZSH_HIGHLIGHT_STYLES[bracket-level-5]=fg=110         # Blue
+ZSH_HIGHLIGHT_STYLES[bracket-error]=fg=167                 # Red
+ZSH_HIGHLIGHT_STYLES[bracket-level-1]=fg=110               # Blue
+ZSH_HIGHLIGHT_STYLES[bracket-level-2]=fg=110               # Blue
+ZSH_HIGHLIGHT_STYLES[bracket-level-3]=fg=110               # Blue
+ZSH_HIGHLIGHT_STYLES[bracket-level-4]=fg=110               # Blue
+ZSH_HIGHLIGHT_STYLES[bracket-level-5]=fg=110               # Blue
 
 # --- Plugin Config End ---
