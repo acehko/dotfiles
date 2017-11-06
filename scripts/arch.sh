46b7922c82c5a515a890667560fe3fc0af421f16
Andrija Čehko <andrija.cehko@gmail.com>
2017-11-06 10:53:50 +0100

    Fix indentation

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -4,26 +4,26 @@
 # Install packages
 echo -e "\nInstalling packages..."
 sudo pacman -S --needed --noconfirm \
-	asciiquarium \
-	clang \
-	cmake \
-	go \
-	make \
-	mosh \
-	neovim \
-	nodejs \
-	npm \
-	python \
-	python-neovim \
-	python2 \
-	python2-neovim \
-	tmux \
-	xclip \
-	yaourt \
-	zsh
+    asciiquarium \
+    clang \
+    cmake \
+    go \
+    make \
+    mosh \
+    neovim \
+    nodejs \
+    npm \
+    python \
+    python-neovim \
+    python2 \
+    python2-neovim \
+    tmux \
+    xclip \
+    yaourt \
+    zsh
 
 
 # Install AUR packages
 echo -e "\nInstalling AUR packages..."
 yaourt -S --needed --noconfirm \
-	neofetch
+    neofetch
