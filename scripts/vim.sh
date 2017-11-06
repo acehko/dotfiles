0ba2f844cad00f8582dbea0fc65fcb2e75b3611e
Andrija Čehko <andrija.cehko@gmail.com>
2017-11-06 10:36:22 +0100

    Clean unused plugins

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -4,32 +4,33 @@
 # Install Vim-Plug
 echo -en "\nInstalling Vim-Plug... "
 if [ ! -e $HOME/.vim/autoload/plug.vim ]; then
-	echo -e "\n"
-	curl -Lo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	echo -e "\n"
+    echo -e "\n"
+    curl -Lo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    echo -e "\n"
 else
-	echo "Already installed"
+    echo "Already installed"
 fi
 
 
 # Symlink neovim config
 echo -en "\nLinking neovim config... "
 if [ -e $HOME/.config/nvim/init.vim ]; then
-	echo "Already exists"
+    echo "Already exists"
 else
 
-	# Create dir if it doesn't exist
-	if [ ! -e $HOME/.config/nvim ]; then
-		mkdir -p $HOME/.config/nvim
-	fi
+    # Create dir if it doesn't exist
+    if [ ! -e $HOME/.config/nvim ]; then
+        mkdir -p $HOME/.config/nvim
+    fi
 
-	ln -s $DOTFILES/vim/nvim.vim $HOME/.config/nvim/init.vim
-	echo -en "\n"
+    ln -s $DOTFILES/vim/nvim.vim $HOME/.config/nvim/init.vim
+    echo -en "\n"
 
 fi
 
 
 # Install plugins
 echo "Installing vim plugins..."
+nvim +PlugClean!
 nvim +PlugUpdate +qall
