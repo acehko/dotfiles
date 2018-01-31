726f98e606b35cf610dcbb5d176e4c8efa5992e7
Andrija Čehko <andrija.cehko@gmail.com>
2018-01-31 22:09:01 +0100

    Zsh completion cache

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+fpath=(~/.zsh/completions $fpath)
+
 # Enable completion
 autoload -Uz compinit && compinit
 
@@ -8,3 +10,7 @@ zstyle ':completion:*' menu select
 
 # Bind SHIFT + TAB for previous selection
 bindkey '^[[Z' reverse-menu-complete
+
+# Use cache
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path ~/.zsh/cache
