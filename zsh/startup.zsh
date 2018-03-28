b37e62d5449f40b5898ac2c08660cc19287f98bc
Andrija Čehko <andrija.cehko@gmail.com>
2018-03-28 13:49:53 +0200

    Remove auto-ssh

--- zsh/startup.zsh
+++ zsh/startup.zsh
@@ -1,27 +1,6 @@
 #!/usr/bin/env zsh
 
-run_ssh() {
-    cmd=$1
-    sess=$2
-
-    host=$(echo "$sess" | cut -d "-" -f 2)
-    if [[ -n $host ]]; then
-        if grep -q "Host $host" "$HOME/.ssh/config"; then
-            $cmd "$host"
-        else
-            echo "Invalid hostname: $host"
-        fi
-    fi
-}
-
-# Running in tmux
-if [[ -n $TMUX ]]; then
-    sess=$(tmux display-message -p "#S")
-    if [[ $sess == ssh-* ]]; then
-        run_ssh "ssh" "$sess"
-    elif [[ $sess == mosh-* ]]; then
-        run_ssh "mosh" "$sess"
-    fi
-else
+# Not running in tmux
+if [[ -z $TMUX ]]; then
     neofetch
 fi
