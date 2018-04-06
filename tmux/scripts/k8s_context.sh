9b69eef5338f3c28c120beb87502cc0567f459df
Andrija Čehko <andrija.cehko@gmail.com>
2018-04-06 15:03:15 +0200

    Fix k8s context script

--- tmux/scripts/k8s_context.sh
+++ tmux/scripts/k8s_context.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 context="$(kubectl config current-context)"
-if [ "$context" != "minikube" ]; then
+if [[ $context != "minikube" && $context != "" ]]; then
     echo "#[fg=colour$COLOR_BLUE,bg=colour$COLOR_BLACK][k8s: $context] "
 fi
