9b3b7808cd60e570465a50521bc1f3a06d80c673
Andrija Čehko <andrija.cehko@gmail.com>
2019-02-13 15:33:37 +0100

    Update fzf colors

--- zsh/config/fzf.zsh
+++ zsh/config/fzf.zsh
@@ -8,8 +8,8 @@ export FZF_DEFAULT_OPTS=$(echo -n "
     --prompt='❯ '
     --border
     --no-bold
-    --color fg:188,bg:$COLOR_BLACK,hl:$COLOR_ORANGE,fg+:$COLOR_BLUE,bg+:$COLOR_BLACK,hl+:$COLOR_ORANGE
-    --color info:$COLOR_GREEN,prompt:$COLOR_BLUE,spinner:$COLOR_GREEN,pointer:$COLOR_BLUE,marker:$COLOR_ORANGE
+    --color hl:5,fg+:$COLOR_BLUE,bg+:$COLOR_BLACK,hl+:5
+    --color info:$COLOR_GREEN,prompt:$COLOR_BLUE,spinner:$COLOR_GREEN,pointer:$COLOR_BLUE,marker:$COLOR_ORANGE,border:$COLOR_BLUE
 ")
 
 export FZF_DEFAULT_COMMAND='rg --files --smart-case --hidden --follow --glob "!.git/*"'
