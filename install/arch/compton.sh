9ca4873ac75baea3c9a8cc732867782c4506d3bb
Andrija Čehko <andrija.cehko@gmail.com>
2019-04-26 23:48:18 +0200

    Add compton installer and config

---
+++ install/arch/compton.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
+set -e
+
+source "$(dirname "$0")/helpers.sh"
+
+echo "Installing compton..."
+AUR \
+    compton-tryone-git
+
+echo
+echo "Linking compton config..."
+sudo bash -c "$(declare -f LINK); LINK '$DOTFILES/compton' 'etc/xdg/compton' 'compton.conf'"
