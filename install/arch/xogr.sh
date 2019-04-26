5dbde6b79c0baedd670406e44fceec5152e9c24b
Andrija Čehko <andrija.cehko@gmail.com>
2019-04-27 01:33:52 +0200

    Create xorg user dirs

--- install/arch/xogr.sh
+++ install/arch/xogr.sh
@@ -6,11 +6,15 @@ source "$(dirname "$0")/helpers.sh"
 
 echo "Installing xorg..."
 PACMAN \
+    xdg-user-dirs \
     xorg-apps \
     xorg-server \
     xorg-xclock \
     xorg-xinit \
     xterm
+
+echo "Creating user dirs..."
+xdg-user-dirs-update
 
 echo
 echo "Linking xorg config..."
