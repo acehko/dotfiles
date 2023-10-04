36345368de13e3c86af24b5b9dc454b6aaa6b0b6
Andrija Čehko <hi@andrija.dev>
2023-10-04 15:34:24 +0200

    Add gitsigns keybindings

--- roles/neovim/templates/lua/plugins/gitsigns.lua
+++ roles/neovim/templates/lua/plugins/gitsigns.lua
@@ -45,6 +45,27 @@ gitsigns.setup({
   yadm = {
     enable = false,
   },
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      neovim.keymap(mode, l, r, opts)
+    end
+
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+  end
 })
 
 neovim.highlight_link('GitSignsAdd', 'Number')
