9f91630e6ca1612dcb2aea8d93cca76234a319f4
Andrija Čehko <hi@andrija.dev>
2023-10-27 13:52:18 +0200

    Fix C-/ keybinding

--- roles/neovim/templates/lua/plugins/commentary.lua
+++ roles/neovim/templates/lua/plugins/commentary.lua
@@ -3,5 +3,5 @@
 local neovim = require('neovim')
 local opts = {remap = true, silent = true}
 
-neovim.keymap('n', '<C-/>', 'gcc', opts)
-neovim.keymap('v', '<C-/>', 'gcc', opts)
+neovim.keymap('n', '<C-_>', 'gcc', opts)
+neovim.keymap('v', '<C-_>', 'gcc', opts)
