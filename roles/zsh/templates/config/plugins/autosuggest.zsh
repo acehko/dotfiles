95bd54d7086bdbe4c819a1695231576ff13619b5
Andrija Čehko <hi@andrija.dev>
2023-10-31 12:18:32 +0100

    Zsh: Update autosuggest keybinding

--- roles/zsh/templates/config/plugins/autosuggest.zsh
+++ roles/zsh/templates/config/plugins/autosuggest.zsh
@@ -3,5 +3,5 @@
 # Autosuggest config.
 
 # Keybindings for accepting and executing autosuggestion.
-bindkey '^ '   autosuggest-accept  # Ctrl + Space
-bindkey '\x99' autosuggest-execute # Ctrl + Return (Hexcode mapped in iTerm2)
+bindkey '^ ' autosuggest-accept  # Ctrl + Space
+bindkey '^M' autosuggest-execute # Ctrl + Return (Hexcode mapped in iTerm2)
